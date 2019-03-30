9c509c9a144dd9e8f60b8e71edb310c1cbe782ce
Gizemm <39413875+gizemhancerlioglu@users.noreply.github.com>
2019-03-30 23:44:07 +0300

    Add files via upload

---
+++ SeleniumTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace UncleSoftware
+{
+    public partial class frmSeleniumTests : Form
+    {
+        public frmSeleniumTests()
+        {
+            InitializeComponent();
+        }
+
+        Testler tst = new Testler();
+        private void buttonProfil_Click(object sender, EventArgs e)
+        {
+            tst.ProfilBilgileriniGoruntule();
+            MessageBox.Show("Test başarılı!!","Test Durumu..");
+        }
+
+        private void buttonBilgiDuzen_Click(object sender, EventArgs e)
+        {
+            tst.ProfilBilgileriniDuzenleme();
+            MessageBox.Show("Test başarılı!!","Test Durumu..");
+        }
+
+        private void buttonHesapCikis_Click(object sender, EventArgs e)
+        {
+            tst.HesaptanCikisYapma();
+            MessageBox.Show("Test başarılı!!","Test Durumu..");
+        }
+
+        private void buttonKayitOl_Click(object sender, EventArgs e)
+        {
+            tst.YeniKayitOlusturma();
+            MessageBox.Show("Test başarılı!!", "Test Durumu..");
+        }
+    }
+}
